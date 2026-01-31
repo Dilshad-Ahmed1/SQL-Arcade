9c8486b57d465947aecdf75130616046e4f6dc0b
Bhoomika Sundar <008bhoomikasundar@gmail.com>
2026-02-01 00:37:26 +0530

    updated for deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,20 @@
 import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+
+const caPath = path.resolve(process.cwd(), process.env.SSL_PATH || '');
 
 export const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD ,
+  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  multipleStatements: true
+  multipleStatements: true,
+  ssl: {
+    ca: fs.readFileSync(caPath)
+  },
+  connectTimeout: 60000
 });
